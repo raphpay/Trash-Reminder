62f87ac5934276edc978cff242563abf3f891c7b
Raphaël Payet <raphaelpayet@macbook-pro-m1-de-raphael.home>
2022-02-05 12:15:51 +0100

    Initial commit

---
+++ Trash Reminder/Trash_ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Trash_ReminderApp.swift
+//  Trash Reminder
+//
+//  Created by Raphaël Payet on 05/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct Trash_ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
