8b3c741382e207c536f44e02b78a443e63d08f90
Raphaël Payet <raphaelpayet@macbook-pro-m1-de-raphael.home>
2022-02-05 12:32:40 +0100

    Add custom fonts

---
+++ Trash Reminder/Support/Font/Font.swift
@@ -0,0 +1,13 @@
+//
+//  Font.swift
+//  Trash Reminder
+//
+//  Created by Raphaël Payet on 05/02/2022.
+//
+
+import Foundation
+
+enum Font: String {
+    case texturina = "Texturina"
+    case texturinaItalic = "Texturina-Italic"
+}
--- Trash Reminder/View/ContentView.swift
+++ Trash Reminder/View/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Trash Reminder")
+                .font(.custom(Font.texturina.rawValue, size: 30))
+            Spacer()
+        }
     }
 }
 
