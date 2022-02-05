fe8eb26b5b033502774979bf0bb89e90805cec74
Raphaël Payet <raphaelpayet@macbook-pro-m1-de-raphael.home>
2022-02-05 12:38:21 +0100

    Add assets

---
+++ Trash Reminder/Utils/Assets.swift
@@ -0,0 +1,13 @@
+//
+//  Assets.swift
+//  Trash Reminder
+//
+//  Created by Raphaël Payet on 05/02/2022.
+//
+
+import Foundation
+
+enum Assets {
+    static let grayTrash = "gray-trash"
+    static let yellowTrash = "yellow-trash"
+}
---
+++ Trash Reminder/Utils/Color+Ext.swift
@@ -0,0 +1,18 @@
+//
+//  Color+Ext.swift
+//  Trash Reminder
+//
+//  Created by Raphaël Payet on 05/02/2022.
+//
+
+import SwiftUI
+
+extension Color {
+    static let ui = Color.Custom()
+    
+    struct Custom {
+        let gray     = Color("Gray")
+        let yellow    = Color("Yellow")
+    }
+}
+
