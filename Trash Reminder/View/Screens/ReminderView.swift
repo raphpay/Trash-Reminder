b07eb5e42643301ca4081a5521f74d90163f307c
raphpay <41484808+raphpay@users.noreply.github.com>
2022-06-07 08:28:46 +0200

    Feature: Add days reminder into UserDefaults #6

--- Trash Reminder/View/Screens/ReminderView.swift
+++ Trash Reminder/View/Screens/ReminderView.swift
@@ -61,7 +61,7 @@ struct ReminderView: View {
                     .pickerStyle(.automatic)
                     
                     Button {
-                        //
+                        addReminder()
                     } label: {
                         ZStack {
                             RoundedRectangle(cornerRadius: 10, style: .continuous)
@@ -77,6 +77,45 @@ struct ReminderView: View {
             }
             .offset(y: showAlert ? 0 : 1000)
             .animation(.spring(), value: showAlert)
+        }
+    }
+    
+    func addReminder() {
+        // TODO: To be refactored
+        // TODO: Remove print statements
+        switch trashSelection {
+        case 0:
+            if var array = UserDefaults.standard.array(forKey: GRAY_TRASH_DAYS_KEY) as? [Int] {
+                print("Gray already created", array.contains(where: { $0 == daySelection }))
+                if array.contains(where: { $0 == daySelection }) {
+                    print("Gray contains the day ==> Error")
+                } else {
+                    array.append(daySelection)
+                    UserDefaults.standard.set(array, forKey: GRAY_TRASH_DAYS_KEY)
+                    print("Gray don't contains the day. New array =", array)
+                }
+            } else {
+                UserDefaults.standard.set([daySelection], forKey: GRAY_TRASH_DAYS_KEY)
+                print("Gray not created")
+            }
+            break
+        case 1:
+            if var array = UserDefaults.standard.array(forKey: YELLOW_TRASH_DAYS_KEY) as? [Int] {
+                print("Yellow already created", array)
+                if array.contains(where: { $0 == daySelection }) {
+                    print("Yellow contains the day ==> Error")
+                } else {
+                    array.append(daySelection)
+                    UserDefaults.standard.set(array, forKey: YELLOW_TRASH_DAYS_KEY)
+                    print("Yellow don't contains the day. New array =", array)
+                }
+            } else {
+                UserDefaults.standard.set([daySelection], forKey: YELLOW_TRASH_DAYS_KEY)
+                print("Yellow already created")
+            }
+            break
+        default:
+            break
         }
     }
 }
